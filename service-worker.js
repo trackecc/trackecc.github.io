af5c942b1ca870c8647e345830eea0df7f40ae86
trackecc <65176953+trackecc@users.noreply.github.com>
2020-05-13 16:28:16 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76674693d0229537dc02fa791b397e96.js"
+  "/precache-manifest.a9f8b98c3e2c6864a626d0fb7dabfe5b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "trackecc"});
